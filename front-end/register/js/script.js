cecf99369f63e118459ca765db0707987f786764
nikizhelqzkov <nikizhelqzkov@gmail.com>
2022-12-19 00:34:53 +0200

    Added submit js logic in rester page Added onsubmit js logic in register page, which adds basic errors next to the input if it has errors and if it is ok adds message for success.

--- front-end/register/js/script.js
+++ front-end/register/js/script.js
@@ -1,10 +1,10 @@
-const email = "nikizhelqzkov@gmail.com";
-const password = "dsafcsal2W@";
-const confirmPassword = "dsafcsal2W@";
-const firstName = "Nikolay";
-const lastName = "Zhelqzkov";
-const fn = "1284213";
-const speciality = "Software Engineering";
+// const email = "nikizhelqzkov@gmail.com";
+// const password = "dsafcsal2W@";
+// const confirmPassword = "dsafcsal2W@";
+// const firstName = "Nikolay";
+// const lastName = "Zhelqzkov";
+// const fn = "1284213";
+// const speciality = "Software Engineering";
 
 const isEmpty = (value) =>
   value === "" || value === undefined || value === null;
@@ -34,28 +34,39 @@ const isValidName = (name) => {
   return !isEmpty(name) && name.length >= 2 && name.length <= 64;
 };
 
-const validate = (email,password,confirmPassword,firstName,lastName,fn,speciality) => {
+const validate = (
+  email,
+  password,
+  confirmPassword,
+  firstName,
+  lastName,
+  fn,
+  speciality
+) => {
   const response = [];
   if (!isValidEmail(email)) {
-    response.push({ type: "email", message: "Invalid email" });
+    response.push({ id: "email", message: "Invalid email" });
   }
   if (!isValidPassword(password)) {
-    response.push({ type: "password", message: "Invalid password" });
+    response.push({ id: "password", message: "Invalid password" });
   }
   if (!isSamePassword(password, confirmPassword)) {
-    response.push({ type: "password", message: "Passwords are not the same" });
+    response.push({
+      id: "confirm_password",
+      message: "Passwords are not the same",
+    });
   }
   if (!isValidFn(fn)) {
-    response.push({ type: "fn", message: "Invalid fn" });
+    response.push({ id: "fn", message: "Invalid fn" });
   }
   if (!isValidSpeciality(speciality)) {
-    response.push({ type: "speciality", message: "Invalid speciality" });
+    response.push({ id: "speciality", message: "Invalid speciality" });
   }
   if (!isValidName(firstName)) {
-    response.push({ type: "firstName", message: "Invalid firstName name" });
+    response.push({ id: "first_name", message: "Invalid firstName name" });
   }
   if (!isValidName(lastName)) {
-    response.push({ type: "lastName", message: "Invalid lastName name" });
+    response.push({ id: "last_name", message: "Invalid lastName name" });
   }
   if (response.length > 0) {
     response.push({ success: false });
@@ -65,5 +76,58 @@ const validate = (email,password,confirmPassword,firstName,lastName,fn,specialit
   return response;
 };
 
-// const response = validate(email,password,confirmPassword,firstName,lastName,fn,speciality);
-// console.log(response);
+const addErrorMessages = (response) => {
+  response.forEach((element) => {
+    debugger;
+    const input = document.querySelector(`#${element.id}`);
+    if (input != null) {
+      //clear the old messages if it has
+      const oldError = input.parentNode.querySelector(".error");
+      if (oldError !== null) {
+        oldError.remove();
+      }
+      //create new error message
+      const errorElement = document.createElement("span");
+      errorElement.classList.add("error");
+      errorElement.innerText = element.message;
+      //add element after the current input
+      input.parentNode.insertBefore(errorElement, input.nextSibling);
+    }
+  });
+};
+
+const form = document.querySelector("#register_form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  debugger;
+  const email = document.querySelector("#email");
+  const password = document.querySelector("#password");
+  const confirmPassword = document.querySelector("#confirm_password");
+  const firstName = document.querySelector("#first_name");
+  const lastName = document.querySelector("#last_name");
+  const fn = document.querySelector("#fn");
+  const speciality = document.querySelector("#speciality");
+
+  const response = validate(
+    email.value,
+    password.value,
+    confirmPassword.value,
+    firstName.value,
+    lastName.value,
+    fn.value,
+    speciality.value
+  );
+  console.log(response);
+  const success = response.find(
+    (element) => element.success !== undefined
+  )?.success;
+  if (success) {
+    //form.submit();
+    console.log("success");
+    //remove all error messages
+    [...e.target.querySelectorAll(".error")].forEach((el) => el.remove());
+  } else {
+    addErrorMessages(response);
+  }
+});
+
