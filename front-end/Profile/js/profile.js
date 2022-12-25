87e874800408f145750ee1b8c764c75a68662d2f
nikizhelqzkov <nikizhelqzkov@gmail.com>
2022-12-25 22:34:48 +0200

    Added front-end profile design

--- front-end/Profile/js/profile.js
+++ front-end/Profile/js/profile.js
@@ -14,7 +14,40 @@ const getSession = async () => {
   }
 };
 
+const loadDataIntoPage = (profile) => {
+  const headerName = document.querySelector("#header_profile_name");
+  headerName.textContent = `${profile.firstName} ${profile.lastName}`;
+  const firstName = document.querySelector("#first_name");
+  const lastName = document.querySelector("#last_name");
+  const username = document.querySelector("#username");
+  const email = document.querySelector("#email");
+  const fn = document.querySelector("#fn");
+  const speciality = document.querySelector("#speciality");
+  const inAlumni = document.querySelector("#in_alumni_text");
 
+  firstName.value = profile.firstName != "" ? profile.firstName : "";
+  lastName.value = profile.lastName != "" ? profile.lastName : "";
+  username.value = profile.username != "" ? profile.username : "";
+  email.value = profile.email != "" ? profile.email : "";
+  fn.value = profile.fn != "" ? profile.fn : "";
+
+  const options = [...speciality.children];
+  if (profile.speciality != null) {
+    const currentOption = options.find(
+      (opt) => opt.value == profile.speciality
+    );
+    if (currentOption != null) {
+      currentOption.selected = true;
+    }
+  }
+
+
+  if (profile.inAlumni != null) {
+    inAlumni.textContent = profile.inAlumni
+      ? "Вие сте в алумни клуба на ФМИ"
+      : "Вие НЕ сте в алумни клуба на ФМИ";
+  }
+};
 
 window.addEventListener("load", async () => {
   let sessionId = null;
@@ -27,8 +60,6 @@ window.addEventListener("load", async () => {
     return;
   }
 
-
   console.log(sessionObj);
- 
-
+  loadDataIntoPage(sessionObj);
 });
