0dbe67cea4c2d68aac29958297324d420a27142b
Iliyan0411 <ilian.dd00@gmail.com>
2022-12-30 12:12:13 +0200

    Improve session updating

--- back-end/api/handlers/getSession.php
+++ back-end/api/handlers/getSession.php
@@ -2,19 +2,35 @@
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     session_start();
     if (isset($_SESSION['user'])) {
+        require_once '../../db/database.php';
+
+        try {
+            $db = new Database();
+            $connection = $db->getConnection();
+            $statement = $connection->prepare('SELECT in_alumni FROM user WHERE id = :id');
+            $statement->execute(['id' => $_SESSION['user']['id']]);
+            $InAlumniData = $statement->fetch(PDO::FETCH_OBJ);
+
+            if ($_SESSION['user']['inAlumni'] != $InAlumniData->in_alumni) {
+                $_SESSION['user']['inAlumni'] = $InAlumniData->in_alumni;
+            }
+        } catch (PDOException $err) {
+            echo $err->getMessage();
+            http_response_code(500);
+            return json_encode([
+                'status' => 'error',
+                'message' => 'Грешка при вземането на сесия!',
+            ]);
+        }
+
         http_response_code(200);
-        echo json_encode($_SESSION['user']);    
+        echo json_encode($_SESSION['user']);
         return json_encode($_SESSION['user']);
     } else {
         http_response_code(401);
-        echo json_encode([
-            'status' => 'error',
-            'message' => 'Няма активна сесия!',]);
         return json_encode([
             'status' => 'error',
             'message' => 'Няма активна сесия!',
         ]);
     }
 }
-
-?>
