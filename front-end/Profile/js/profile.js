e922f910f18c6eb1c98c2fc52951a423d1b84122
Iliyan0411 <ilian.dd00@gmail.com>
2023-05-24 15:37:17 +0300

    -

--- front-end/Profile/js/profile.js
+++ front-end/Profile/js/profile.js
@@ -191,7 +191,10 @@ form.addEventListener("submit", async (e) => {
         if (updatedProfile.error) {
           throw new Error(data.error);
         }
-        console.log(updatedProfile);
+        if (updatedProfile.profilePictureUrl) {
+          const profilePicture = document.getElementById("profile-picture");
+          profilePicture.innerHTML += '<img src=${updatedProfile.profilePictureUrl}> alt="logo"';
+        }
         [...e.target.querySelectorAll(".error")].forEach((el) => el.remove());
         window.location.reload();
       } else if (response.status == 400) {
